e6a99dfc20cf22d9272b398de706736951fccbe2
Kobby-20 <151926930+Kobby-20@users.noreply.github.com>
2024-06-19 16:33:35 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import LoginScreen from './LoginScreen';
+import HomepageScreen from './HomepageScreen';
+
+export default function App() {
+  const [currentScreen, setCurr
+    entScreen] = useState('Login');
+
+  const handleNavigate = (screen) => {
+    setCurrentScreen(screen);
+  };
+
+  return (
+    <View style={styles.container}>
+      {currentScreen === 'Login' ? (
+        <LoginScreen navigate={handleNavigate} />
+      ) : (
+        <HomepageScreen />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
